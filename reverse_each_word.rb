746e38bcda5ba6b4f4b19863a916a8164350658c
edmundo papi <edmundopierre@yahoo.com>
2020-03-27 11:37:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,19 @@
-# def reverse_each_word(str)
-#
-# ##  p= "Hello there, and how are you?".split
-#   str_array = str.split
-#   empty_array = []
-#   str_array.each do |value|
-#      empty_array <<  value.reverse
-#   end
-#   empty_array.join(" ")
-# end
+def reverse_each_word(str)
+
+##  p= "Hello there, and how are you?".split
+  str_array = str.split
+  empty_array = []
+  str_array.each do |value|
+     empty_array <<  value.reverse
+  end
+  empty_array.join(" ")
+end
 
 def reverse_each_word(str)
     string_array = str.split
-    empt_array = []
+    empty_array = []
     string_array.collect do |value|
-       value.reverse
+       empty_array.push(value.reverse)
     end
+    empty_array.join(" ")
 end
